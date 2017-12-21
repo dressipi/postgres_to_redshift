ab161df5b6d99dbe8c5c47975797b5f1014f7220
Chan Yi Sheng <easonchan1213@gmail.com>
2017-12-21 14:37:50 +0000

    Remove unused code and env variables

--- lib/postgres_to_redshift.rb
+++ lib/postgres_to_redshift.rb
@@ -34,31 +34,22 @@ class PostgresToRedshift
         ddl << ", primary key(#{table.primary_key})" if table.primary_key
         ddl << ')'
         target_connection.exec(ddl)
-        
-        if migrate?
-          copy_table(table: table, schema: schema)
-          import_table(table: table, schema: schema)
-        end
       end
     end
   end
 
-  def source_uri
-    @source_uri ||= URI.parse(ENV['POSTGRES_TO_REDSHIFT_SOURCE_URI'])
-  end
-
   def target_uri
-    @target_uri ||= URI.parse(ENV['POSTGRES_TO_REDSHIFT_TARGET_URI'])
+    @target_uri ||= URI.parse(ENV['REDSHIFT_URI'])
   end
 
   def source_connection
     unless instance_variable_defined?(:"@source_connection")
       @source_connection = PG::Connection.new(
-        host: source_uri.host, 
-        port: source_uri.port, 
-        user: source_uri.user || ENV['USER'], 
-        password: source_uri.password, 
-        dbname: source_uri.path[1..-1])
+        host: ENV.fetch('PGHOST'), 
+        port: 5432, 
+        user: ENV.fetch('PGUSER'), 
+        password: ENV.fetch('PGPASSWORD'), 
+        dbname: ENV.fetch('PGDATABASE'))
       @source_connection.exec("SET SESSION CHARACTERISTICS AS TRANSACTION READ ONLY")
     end
 
@@ -70,20 +61,12 @@ class PostgresToRedshift
       @target_connection = PG::Connection.new(
         host: target_uri.host, 
         port: target_uri.port, 
-        user: target_uri.user || ENV['USER'], 
+        user: target_uri.user, 
         password: target_uri.password, 
         dbname: @dbname)
     end
 
     @target_connection
-  end
-
-  def migrate?
-    if ENV.has_key? 'POSTGRES_TO_REDSHIFT_MIGRATE' and (ENV.fetch('POSTGRES_TO_REDSHIFT_MIGRATE').downcase == 'true')
-      true
-    else
-      false
-    end
   end
 
   def schema_exist?(schema)
@@ -162,85 +145,6 @@ class PostgresToRedshift
   def column_definitions(table:, schema:)
     source_connection.exec('SELECT * FROM information_schema.columns ' \
       "WHERE table_schema='#{schema}' AND table_name='#{table.name}' order by ordinal_position")
-  end
-
-  def s3
-    Aws::S3::Client.new(access_key_id: ENV['S3_DATABASE_EXPORT_ACCESSKEY'], secret_access_key: ENV['S3_DATABASE_EXPORT_SECRETKEY'])
-  end
-
-  def export_s3_bucket
-    ENV['S3_DATABASE_EXPORT_BUCKET']
-  end
-
-  def copy_table(table:, schema:)
-    tmpfile = Tempfile.new("psql2rs")
-    zip = Zlib::GzipWriter.new(tmpfile)
-    chunksize = 10 * GIGABYTE # uncompressed
-    chunk = 0
-    begin
-      puts "Downloading #{table}"
-      copy_command = "COPY (SELECT #{table.columns_for_copy} FROM #{schema}.#{table.name}) TO STDOUT WITH DELIMITER '|'"
-
-      source_connection.copy_data(copy_command) do
-        while row = source_connection.get_copy_data
-          zip.write(row)
-          if (zip.pos > chunksize)
-            zip.finish
-            tmpfile.rewind
-            upload_table(schema, table, tmpfile, chunk)
-            chunk += 1
-            zip.close unless zip.closed?
-            tmpfile.unlink
-            tmpfile = Tempfile.new("psql2rs")
-            zip = Zlib::GzipWriter.new(tmpfile)
-          end
-        end
-      end
-      zip.finish
-      tmpfile.rewind
-      upload_table(schema, table, tmpfile, chunk)
-      source_connection.reset
-    ensure
-      zip.close unless zip.closed?
-      tmpfile.unlink
-    end
-  end
-
-  def upload_table(schema, table, buffer, chunk)
-    puts "Uploading #{schema}.#{table.target_table_name}"
-
-    key = "test/#{schema}.#{table.target_table_name}.psv.gz"
-    key += chunk > 0 ? ".#{chunk}" : ''
-    params = {
-      bucket: export_s3_bucket,
-      key: key,
-      body: buffer
-    }
-
-    s3.put_object(params)
-  end
-
-  def import_table(table:, schema:)
-    puts "Importing #{schema}.#{table.target_table_name}"
-    
-    target_connection.exec("DROP TABLE IF EXISTS #{schema}.#{table.target_table_name}_backup")
-
-    target_connection.exec("BEGIN")
-
-    target_connection.exec('ALTER TABLE ' \
-      "#{schema}.#{target_connection.quote_ident(table.target_table_name)} RENAME TO " \
-      "#{table.target_table_name}_backup")
-
-    target_connection.exec('CREATE TABLE ' \
-      "#{schema}.#{target_connection.quote_ident(table.target_table_name)} " \
-      "(#{table.columns_for_create})")
-
-    target_connection.exec("COPY #{schema}.#{target_connection.quote_ident(table.target_table_name)} " \
-      "FROM 's3://#{export_s3_bucket}/test/#{schema}.#{table.target_table_name}.psv.gz' " \
-      "CREDENTIALS 'aws_access_key_id=#{ENV['S3_DATABASE_EXPORT_ID']};aws_secret_access_key=#{ENV['S3_DATABASE_EXPORT_KEY']}' " \
-      "GZIP TRUNCATECOLUMNS ESCAPE DELIMITER as '|'")
-
-    target_connection.exec("COMMIT")
   end
 
   def close_connections
